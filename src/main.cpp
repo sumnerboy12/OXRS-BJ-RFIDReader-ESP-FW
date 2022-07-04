5d62410ce68bfc5db6fbb971fb784deff690758a
Ben Jones <sumnerboy12@users.noreply.github.com>
2022-07-04 16:13:51 +1200

    Switch to using PN532 reader (#4)

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,18 @@
 #define STRINGIFY1(s) #s
 
 /*--------------------------- Libraries -------------------------------*/
+#include <SoftwareSerial.h>
+#include <NfcAdapter.h>
+#include <PN532/PN532/PN532.h>
+
+#ifdef USE_I2C_NFC
+#include <Wire.h>
+#include <PN532/PN532_I2C/PN532_I2C.h>
+#else
 #include <SPI.h>
-#include <MFRC522.h>
+#include <PN532/PN532_SPI/PN532_SPI.h>
+#endif
+
 #include <WiFiManager.h>
 #include <OXRS_MQTT.h>
 #include <OXRS_API.h>
@@ -23,10 +33,16 @@
 
 /*--------------------------- Constants -------------------------------*/
 // Serial
-#define     SERIAL_BAUD_RATE          115200
+#define     SERIAL_BAUD_RATE              115200
 
 // REST API
-#define     REST_API_PORT             80
+#define     REST_API_PORT                 80
+
+// Time between tag reads
+#define     DEFAULT_TAG_READ_INTERVAL_MS  200
+
+// Max NFC tag UID length
+#define     MAX_UID_BYTES                 8
 
 /*--------------------------- Global Variables ------------------------*/
 // stack size counter
@@ -48,7 +64,18 @@ OXRS_API _api(_mqtt);
 MqttLogger _logger(_mqttClient, "log", MqttLoggerMode::MqttAndSerial);
 
 // RFID reader
-MFRC522 mfrc522(SPI_CS_PIN, SPI_RST_PIN);
+#ifdef USE_I2C_NFC
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#else
+PN532_SPI pn532_spi(SPI, SPI_SS_PIN);
+NfcAdapter nfc = NfcAdapter(pn532_spi);
+#endif
+
+// Last tag read and when
+uint32_t tagReadIntervalMs = DEFAULT_TAG_READ_INTERVAL_MS;
+uint32_t lastTagReadMs = 0L;
+byte lastUid[MAX_UID_BYTES];
 
 /*--------------------------- Program ---------------------------------*/
 uint32_t getStackSize()
@@ -57,95 +84,107 @@ uint32_t getStackSize()
   return (uint32_t)stackStart - (uint32_t)&stack;  
 }
 
-char * toHexString(char buffer[], byte data[], unsigned int len)
+char * toHexString(char buffer[], byte data[], uint8_t len)
 {
-  for (unsigned int i = 0; i < len; i++)
+  for (uint8_t i = 0; i < len; i++)
   {
     byte nib1 = (data[i] >> 4) & 0x0F;
     byte nib2 = (data[i] >> 0) & 0x0F;
 
-    buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
-    buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1 - 0xA;
+    buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2 - 0xA;
   }
 
   buffer[len*2] = '\0';
   return buffer;
 }
 
-unsigned int readData_MIFARE_UL(byte data[])
+char * toAsciiString(char buffer[], byte data[], uint8_t len)
 {
-  MFRC522::StatusCode status;
-
-  // 16 (data) + 2 (CRC)
-  byte buffer[18];
-  byte byteCount = sizeof(buffer);
-
-  int dataCount = 0;
-
-  // Each read is 4 pages of 4 bytes - we skip the first 4 pages as that is the UID
-  for (byte page = 4; page <= 15; page += 4)
+  for (uint8_t i = 0; i < len; i++)
   {
-    status = mfrc522.MIFARE_Read(page, buffer, &byteCount);
-    if (status != mfrc522.STATUS_OK)
+    if (data[i] <= 0x1F) 
     {
-      _logger.print(F("MIFARE_Read() failed: "));
-      _logger.println(mfrc522.GetStatusCodeName(status));
-      break;
-    }
-
-    for (int i = 0; i <= 3; i++)
+      buffer[i] = '.';
+    } 
+    else 
     {
-      for (int j = 0; j <= 3; j++)
-      {
-        data[dataCount++] = buffer[4 * i + j];
-      }
+      buffer[i] = (char)data[i];
     }
   }
 
-  return dataCount;
+  buffer[len] = '\0';
+  return buffer;
 }
 
-void publishCard(MFRC522::Uid uid)
+void publishTag(NfcTag * tag)
 {
-  char buffer[128];
-  byte data[64];
-  unsigned int data_len = 0;
+  // get the tag UID
+  byte uid[MAX_UID_BYTES];
+  tag->getUid(uid, tag->getUidLength());
 
-  // Get the card type
-  MFRC522::PICC_Type type = mfrc522.PICC_GetType(uid.sak);
+  // build the JSON payload with the tag details
+  DynamicJsonDocument json(4096);
+  char buffer[1024];
 
-  // Attempt to read any data from supported card types
-  if (type == MFRC522::PICC_TYPE_MIFARE_UL)
+  json["uid"] = toHexString(buffer, uid, tag->getUidLength());
+  json["type"] = tag->getTagType();
+
+  // does this tag have a message?
+  if (tag->hasNdefMessage())
   {
-    data_len = readData_MIFARE_UL(data);
+    NdefMessage ndefMessage = tag->getNdefMessage();
+
+    JsonArray recordsJson = json.createNestedArray("records");
+    for (uint8_t i = 0; i < ndefMessage.getRecordCount(); i++)
+    {
+      NdefRecord ndefRecord = ndefMessage.getRecord(i);
+
+      int payloadLength = ndefRecord.getPayloadLength();
+      byte payload[payloadLength];
+      ndefRecord.getPayload(payload);
+
+      JsonObject recordJson = recordsJson.createNestedObject();
+      recordJson["tnf"] = ndefRecord.getTnf();
+      recordJson["type"] = ndefRecord.getType();
+      recordJson["id"] = ndefRecord.getId();
+      recordJson["bytes"] = ndefRecord.getEncodedSize();
+
+      JsonObject payloadJson = recordJson.createNestedObject("payload");
+      payloadJson["hex"] = toHexString(buffer, payload, payloadLength);
+      payloadJson["ascii"] = toAsciiString(buffer, payload, payloadLength);
+    }
   }
 
-  // Build the JSON payload
-  StaticJsonDocument<256> json;
-
-  json["sak"] = uid.sak;
-  json["type"] = mfrc522.PICC_GetTypeName(type);
-  json["uid"] = toHexString(buffer, uid.uidByte, uid.size);
-  json["data"] = toHexString(buffer, data, data_len);
-
+  // publish the tag details to MQTT
   _mqtt.publishStatus(json.as<JsonVariant>());
 }
 
-void processMFRC522() 
+void processPN532() 
 {
-  // Only interested in new card reads (i.e. don't loop if a card is held over the reader)
-  if (!mfrc522.PICC_IsNewCardPresent())
+  // if no tag present then ensure we are ready to read a new one
+  if (!nfc.tagPresent(5))
+  {
+    memset(lastUid, 0, MAX_UID_BYTES);
+    return;
+  }
+
+  // read the tag details
+  NfcTag tag = nfc.read();
+
+  // get the tag UID
+  byte uid[MAX_UID_BYTES];
+  tag.getUid(uid, tag.getUidLength());
+
+  // if the tag hasn't changed then nothing to do
+  if (memcmp(uid, lastUid, tag.getUidLength()) == 0) 
     return;
 
-  // Verify the card has been read
-  if (!mfrc522.PICC_ReadCardSerial())
-    return;
+  // save the tag UID so we can ignore re-reads
+  memcpy(lastUid, uid, tag.getUidLength());
 
-  // Publish the details of the card
-  publishCard(mfrc522.uid);
-
-  // Halt reader
-  mfrc522.PICC_HaltA();
+  // publish the tag details
+  publishTag(&tag);
 }
 
 void getFirmwareJson(JsonVariant json)
@@ -205,6 +244,13 @@ void getConfigSchemaJson(JsonVariant json)
   configSchema["type"] = "object";
 
   JsonObject properties = configSchema.createNestedObject("properties");
+
+  JsonObject tagReadIntervalMs = properties.createNestedObject("tagReadIntervalMs");
+  tagReadIntervalMs["title"] = "Tag Read Interval (milliseconds)";
+  tagReadIntervalMs["description"] = "How often to check if a tag is near the reader (defaults to 200 milliseconds). Must be a number between 0 and 60000 (i.e. 1 min).";
+  tagReadIntervalMs["type"] = "integer";
+  tagReadIntervalMs["minimum"] = 0;
+  tagReadIntervalMs["maximum"] = 60000;
 }
 
 void getCommandSchemaJson(JsonVariant json)
@@ -294,6 +340,10 @@ void _mqttCallback(char * topic, byte * payload, int length)
 
 void _mqttConfig(JsonVariant json)
 {
+  if (json.containsKey("tagReadIntervalMs"))
+  {
+    tagReadIntervalMs = json["tagReadIntervalMs"].as<uint32_t>();
+  }
 }
 
 void _mqttCommand(JsonVariant json)
@@ -378,13 +428,20 @@ void initialiseRestApi(void)
   _server.begin();
 }
 
-void initialiseMFRC522(void)
+void initialisePN532(void)
 {
-  // Start the SPI bus
-  SPI.begin();
+  _logger.print("[rfid] scanning for NFC reader on ");
 
-  // Initialise the MFRC522 card
-  mfrc522.PCD_Init();
+#ifdef USE_I2C_NFC
+  _logger.println(F("I2C"));
+  Wire.begin();
+#else
+  _logger.println(F("SPI"));
+  SPI.begin();
+#endif
+
+  // Initialise the PN532 reader
+  nfc.begin();
 }
 
 /**
@@ -410,8 +467,8 @@ void setup()
   // Set up the REST API
   initialiseRestApi();
 
-  // Set up the MFRC522 RFID reader
-  initialiseMFRC522();
+  // Set up the RFID reader
+  initialisePN532();
 }
 
 /**
@@ -426,6 +483,13 @@ void loop()
   WiFiClient client = _server.available();
   _api.loop(&client);
 
-  // Process RFID reader
-  processMFRC522();
+  // Check if we are ready to read another tag
+  if ((millis() - lastTagReadMs) > tagReadIntervalMs)
+  {
+    // Process RFID reader
+    processPN532();
+
+    // Reset our timer
+    lastTagReadMs = millis();
+  }
 }
